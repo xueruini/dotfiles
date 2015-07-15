697cdd70dd5404a671eb39113da8dc8052ada7f9
Ruini Xue <xueruini@gmail.com>
2015-07-15 19:04:37 +0800

    Remove text for md.

--- emacs/init.el
+++ emacs/init.el
@@ -155,7 +155,6 @@ This command is convenient when reading novel, documentation."
 ;; (package-install 'markdown-mode)
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
